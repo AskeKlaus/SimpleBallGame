a37640c7ac1054da3235eb295e1fb2de664c74ce
Aske <klaus.aske@yandex.ru>
2023-02-03 12:50:12 -0600

    Working on camera movement
    Added different logic

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private GameObject player;
+    private Rigidbody cameraRB;
     private Vector3 rotation;
+    private Vector3 oldPosition;
+    private Vector3 newPosition;
     private int numberOfOffset;
     [SerializeField] private Vector3[] offsets = new Vector3[4];
     void Start()
     {
         player = GameObject.Find("Player");
+        cameraRB = GetComponent<Rigidbody>();
         numberOfOffset = 0;
     }
 
@@ -34,7 +38,9 @@ public class CameraController : MonoBehaviour
             else numberOfOffset = offsets.Length - 1;
             AdjustRotation();
         }
-        transform.position = player.transform.position + offsets[numberOfOffset];
+        oldPosition = transform.position;
+        newPosition = player.transform.position + offsets[numberOfOffset];
+
 
 
     }
@@ -44,23 +50,23 @@ public class CameraController : MonoBehaviour
         switch (numberOfOffset)
         {
             case 0:
-                rotation = new Vector3(0, 0, 0);
+                rotation = new Vector3(60, 0, 0);
                 break;
 
             case 1:
-                rotation = new Vector3(0, -90, 0);
+                rotation = new Vector3(60, -90, 0);
                 break;
 
             case 2:
-                rotation = new Vector3(0, -180, 0);
+                rotation = new Vector3(60, -180, 0);
                 break;
 
             case 3:
-                rotation = new Vector3(0, -270, 0);
+                rotation = new Vector3(60, -270, 0);
                 break;
 
             default:
-                rotation = new Vector3(0, 0, 0);
+                rotation = new Vector3(60, 0, 0);
                 break;
         }
         transform.eulerAngles = rotation;
--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -3,21 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Counter : MonoBehaviour
 {
-    public Text CounterText;
+    public TextMeshProUGUI counterText;
 
-    private int Count = 0;
+    private int count = 0;
 
     private void Start()
     {
-        Count = 0;
+        count = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Count += 1;
-        CounterText.text = "Count : " + Count;
+        if (other.CompareTag("PlusPoint"))
+        {
+            count += 1;
+            counterText.text = "Count : " + count + "/10";
+        }
+
     }
 }
