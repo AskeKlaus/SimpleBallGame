5b189ceb6dcb5003e885876e427168981d542c77
Aske <klaus.aske@yandex.ru>
2023-02-03 22:33:45 -0600

    Tried change way camera works
    Didn't really achieved anything, need to try again

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,42 +7,30 @@ public class CameraController : MonoBehaviour
     private GameObject player;
     private Rigidbody cameraRB;
     private Vector3 rotation;
-    private Vector3 oldPosition;
-    private Vector3 newPosition;
+    //private Vector3 oldPosition;
+    //private Vector3 newPosition;
     private int numberOfOffset;
+    //public bool moveCameraIsActive;
     [SerializeField] private Vector3[] offsets = new Vector3[4];
+    //[SerializeField] private float speed = 1.0f;
     void Start()
     {
         player = GameObject.Find("Player");
-        cameraRB = GetComponent<Rigidbody>();
+        //cameraRB = GetComponent<Rigidbody>();
         numberOfOffset = 0;
+        //moveCameraIsActive = false;
+
     }
 
     void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (numberOfOffset < offsets.Length - 1)
-            {
-                numberOfOffset++;
-            }
-            else numberOfOffset = 0;
-            AdjustRotation();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (numberOfOffset > 0)
-            {
-                numberOfOffset--;
-            }
-            else numberOfOffset = offsets.Length - 1;
-            AdjustRotation();
-        }
-        oldPosition = transform.position;
-        newPosition = player.transform.position + offsets[numberOfOffset];
-
-
-
+        //oldPosition = transform.position;
+        //newPosition = player.transform.position + offsets[numberOfOffset];
+        //if (!moveCameraIsActive)
+        //{
+        transform.position = player.transform.position + offsets[numberOfOffset];
+        // }
+        ChangeView();
     }
 
     void AdjustRotation()
@@ -71,4 +59,47 @@ public class CameraController : MonoBehaviour
         }
         transform.eulerAngles = rotation;
     }
+    void ChangeView()
+    {
+        //moveCameraIsActive = true;
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (numberOfOffset < offsets.Length - 1)
+            {
+                numberOfOffset++;
+            }
+            else numberOfOffset = 0;
+            AdjustRotation();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (numberOfOffset > 0)
+            {
+                numberOfOffset--;
+            }
+            else numberOfOffset = offsets.Length - 1;
+            AdjustRotation();
+
+            //StartCoroutine(MoveCamera());
+        }
+    }
+
+    /*IEnumerator MoveCamera()
+    {
+        var step = speed * Time.deltaTime;
+
+
+        while (oldPosition != newPosition)
+        {
+            transform.position = Vector3.Lerp(oldPosition, newPosition, step);
+            if(Vector3.Distance(transform.position, newPosition) < 0.17)
+            {
+                transform.position = newPosition;
+            }
+            yield return null;
+        }
+        moveCameraIsActive = false;
+    }*/
+
+
 }
